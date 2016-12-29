ff25d59340cc2bb01b86a03e91a79d2331c037fa
Pavel <pavelluz@Pavels-MacBook-Pro-2.local>
2016-12-29 18:09:21 +0300

    continue with MPJ

--- Higher_order/obj_prototype.js
+++ Higher_order/obj_prototype.js
@@ -1,13 +1,27 @@
 const cat = {
-  makeSound: function () {
-    console.log(this.sound)
-  }
+  init: function (sound) {
+    this.sound = sound
+    return this
+  },
+    makeSound: function () {
+      console.log(this.sound);
+    }
 }
 
+// function objectCreate(proto) {  // создает новый объект
+//   const obj = {} // ≤ - этот
+//   Object.setPrototypeOf(obj, proto) // с прототипом установленным
+//   return obj            // нв этот объект(proto)
+// }
+
+
 const mark = Object.create(cat)
-mark.sound = 'meeeeowwww'
+mark.init = ('meeeeowwww')
 mark.makeSound()
 
-const waffles = Object.create(cat)
-waffles.sound = 'Mrrrrrrrr'
+const waffles =
+  Object.create(cat).init('Mrrrrrrrr')
 waffles.makeSound()
+
+console.log('is mark a cat?',
+cat.isPrototypeOf(mark));
